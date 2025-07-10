71c56cb1385062d2234ff37f5c8ff4aab7f4c512
Eliabr Rodrigues <eliabe859@gmail.com>
2025-07-10 13:42:04 -0300

    feat: add rooms listing

---
+++ src/http/routes/get-rooms.ts
@@ -0,0 +1,12 @@
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod"
+import { db } from "../../db/connection.ts"
+import { schema } from "../../db/schema/index.ts"
+import { id } from "zod/v4/locales"
+
+export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
+  app.get('/rooms', async () => {
+    const results = await db.select({id: schema.rooms.id, name: schema.rooms.name}).from(schema.rooms).orderBy(schema.rooms.createAt)
+
+    return results
+  })
+}
